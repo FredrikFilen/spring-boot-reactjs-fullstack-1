c194d248265eb3d19b6f953416f3cd1601670b3b
FredrikFilen <69147419+FredrikFilen@users.noreply.github.com>
2021-04-29 09:15:38 +0200

    first commit

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,16 +1,56 @@
-import React, { Component } from "react";
+import React, {Component, useCallback} from "react";
 import ReactDOM from "react-dom";
-
-
+import Header from '../../../frontend/src/components/layout/Header';
+import RegisterUser from '../../../frontend/src/components/RegisterUser';
+import Users from '../../../frontend/src/components/Users';
+import axios from "axios";
 export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users:[
+            ]
+        }
+    }
+
+    componentDidMount() {
+        axios.get('/user/all')
+            .then(response => this.setState({users:response.data}))
+    }
+
+    //Deleting User
+    removeUser = (id) =>{
+        axios.delete(`/user/${id}`)
+            .then(
+                response =>this.setState( //Updating UI
+                    {users: [...this.state.users.filter(
+                            user => user.id !== id
+                        )]}
+                )
+            );
+    }
+
+    addUser = (newUser) =>{
+        axios.post('/user/save',newUser)
+            .then(
+                (response) =>{
+                    console.log(response.data);
+                    this.setState({users:[...this.state.users,response.data]})
+                }
+            );
+    }
+
     render() {
         return (
-            <div>
-            <h1>Welcome to React Front End Served by Spring Boot</h1>
-        </div>
-    );
+            <div className="container">
+                <Header/>
+                <RegisterUser addUser={this.addUser}/>
+                <Users users={this.state.users} removeUser={this.removeUser}/>
+            </div>
+        );
     }
 }
+
 
 export default App;
 
